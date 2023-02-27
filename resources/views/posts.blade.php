93404dddec1d31af17a5231accd831ac6354d90d
mahipalramani <mnramani321@gmail.com>
2023-02-27 15:32:59 +0530

    complted

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    
+    <article>
+        <h1><a href="/posts/my-first-post"> My First post</a></h1>
+       
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam voluptas earum optio fuga expedita ducimus? Doloribus, illum perspiciatis, dicta praesentium eveniet dolorem id quisquam excepturi doloremque repellendus quis maiores!</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post"> My Second post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam voluptas earum optio fuga expedita ducimus? Doloribus, illum perspiciatis, dicta praesentium eveniet dolorem id quisquam excepturi doloremque repellendus quis maiores!</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post"> My Third post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam voluptas earum optio fuga expedita ducimus? Doloribus, illum perspiciatis, dicta praesentium eveniet dolorem id quisquam excepturi doloremque repellendus quis maiores!</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-forth-post"> My Forth post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam voluptas earum optio fuga expedita ducimus? Doloribus, illum perspiciatis, dicta praesentium eveniet dolorem id quisquam excepturi doloremque repellendus quis maiores!</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-fifth-post"> My Forth post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam voluptas earum optio fuga expedita ducimus? Doloribus, illum perspiciatis, dicta praesentium eveniet dolorem id quisquam excepturi doloremque repellendus quis maiores!</p>
+    </article>
+</body>
+</html>
